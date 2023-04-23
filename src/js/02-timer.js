b2afbaf3cbd82c709f753031ad102c8a6923f5cd
michalmazurdev <m.mazur40@gmail.com>
2023-04-23 15:31:05 +0200

    2nd task code made cleaner

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,28 +2,25 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 const inputEl = document.getElementById('datetime-picker');
 const buttonEl = document.querySelector('button[data-start]');
+const daysCounterEl = document.querySelector('span[data-days]');
+const hoursCounterEl = document.querySelector('span[data-hours]');
+const minutesCounterEl = document.querySelector('span[data-minutes]');
+const secondsCounterEl = document.querySelector('span[data-seconds]');
 buttonEl.disabled = true;
 function addLeadingZero(value) {
   if (value.length <= 2) {
-    return value.padStart(2, '0');
+    return value.toString().padStart(2, '0');
   }
 }
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
 let chosenDate = null;
@@ -48,18 +45,10 @@ buttonEl.addEventListener('click', () => {
   let timeLeft = {};
   function updateTimer() {
     timeLeft = convertMs(chosenDate.getTime() - new Date().getTime());
-    document.querySelector('span[data-days]').innerText = addLeadingZero(
-      timeLeft.days.toString()
-    );
-    document.querySelector('span[data-hours]').innerText = addLeadingZero(
-      timeLeft.hours.toString()
-    );
-    document.querySelector('span[data-minutes]').innerText = addLeadingZero(
-      timeLeft.minutes.toString()
-    );
-    document.querySelector('span[data-seconds]').innerText = addLeadingZero(
-      timeLeft.seconds.toString()
-    );
+    daysCounterEl.innerText = addLeadingZero(timeLeft.days);
+    hoursCounterEl.innerText = addLeadingZero(timeLeft.hours);
+    minutesCounterEl.innerText = addLeadingZero(timeLeft.minutes);
+    secondsCounterEl.innerText = addLeadingZero(timeLeft.seconds);
   }
   inputEl.disabled = true;
   updateTimer();
