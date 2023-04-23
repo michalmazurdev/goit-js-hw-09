76bfbcc22ba4c58964d266dcc9851bdf9b3eacd4
michalmazurdev <m.mazur40@gmail.com>
2023-04-23 02:05:27 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEL = document.querySelector('button[data-stop]');
+let intervalId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+startBtnEl.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnEl.setAttribute('disabled', true);
+  stopBtnEL.removeAttribute('disabled');
+});
+stopBtnEL.addEventListener('click', () => {
+  clearInterval(intervalId);
+  stopBtnEL.setAttribute('disabled', true);
+  startBtnEl.removeAttribute('disabled');
+});
