d731c03151402b0f377707001cc1620b96d09ae6
michalmazurdev <117107541+michalmazurdev@users.noreply.github.com>
2023-04-23 00:06:07 +0000

    Deploying to gh-pages from @ michalmazurdev/goit-js-hw-09@76bfbcc22ba4c58964d266dcc9851bdf9b3eacd4 🚀

---
+++ 01-color-switcher.6cd04422.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(()=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(()=>{clearInterval(o),r.setAttribute("disabled",!0),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.6cd04422.js.map
